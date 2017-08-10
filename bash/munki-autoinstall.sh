4ac733d40c8b1d653b64d484f2c5ff53522c5e8c
Matthew Vogel <matthew@apnic.net>
2017-08-10 16:54:58 +1000

    just a few scripts

---
+++ bash/munki-autoinstall.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Download and install the latest Munki 3 tools from munkibuilds.org
+
+cat <<EOF
+
+**************************
+** Munki auto-installer **
+**************************
+
+EOF
+if [ "$(id -u)" -ne 0 ]; then
+    echo "(Please enter your sudo password when prompted)"
+    echo ""
+fi
+
+tmpdir=$(mktemp -d /tmp/munkibuilds-XXXX)
+pkg_download="${tmpdir}/munki3.pkg"
+echo "Grabbing the latest version..."
+curl \
+    -s \
+    -f \
+    -o "${pkg_download}" \
+    --connect-timeout 30 \
+    https://munkibuilds.org/munkitools3-latest.pkg
+
+sudo /usr/sbin/installer -pkg "${pkg_download}" -target /
+
