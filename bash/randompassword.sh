97a98d26c8383adad91cf29a6a0c2adde8a8c4d2
Matt Vogel <matt.vogel@temando.com>
2019-04-04 13:37:55 +1000

    updated random password gen so that it actually works

--- bash/randompassword.sh
+++ bash/randompassword.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -Is https://en.wikipedia.org/wiki/Special:Random | grep Location | awk -F'/' '{ print tolower() }' | sed 's/_//g' | sed 's/[^a-zA-Z]//g'
+curl -Is https://en.wikipedia.org/wiki/Special:Random |grep location |awk -F'/' '{ print tolower($5) }'|sed 's/_//g' | sed 's/[^a-zA-Z]//g'
